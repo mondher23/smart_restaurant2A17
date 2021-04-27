4a1d651a610f4346ef0ea771bfa6ad28e7bbbd10
MAzizBenSalem <mohamedaziz.bensalem@esprit.tn>
2021-04-27 15:29:35 +0100

    Aziz commit

---
+++ GestionClients/client.h
@@ -0,0 +1,39 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <QString>
+#include <QSqlQueryModel>
+
+class Client
+{
+public:
+    Client();
+    Client(int,QString,QString,QString,QString,int);
+
+    int  getcinc();
+    QString getnom();
+    QString getprenom();
+    QString getmail();
+    QString getadresse();
+    int gettel();
+
+    void setcinc(int);
+    void setnom(QString);
+    void setprenom(QString);
+    void setmail(QString);
+    void setadresse(QString);
+    void settel(int);
+
+    bool ajouterClient();
+    QSqlQueryModel* afficherClient();
+    bool supprimerClient(int);
+    bool modifierClient(int);
+    QSqlQueryModel* trierClient();
+    QSqlQueryModel* rechercherClient(int);
+
+
+private:
+    int cinc,tel;
+    QString nom,prenom,mail,adresse;
+};
+
+#endif // CLIENT_H
